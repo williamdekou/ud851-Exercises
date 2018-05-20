fba7c7f4c82950a4e593fc6403ec849525e7ef4e
William DEKOU <williamdekou@gmail.com>
2018-05-20 11:06:18 +0100

    Fix unchecked TODO

--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -22,28 +22,28 @@ import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
+    // TODO (1)(ok) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
     private static final int NUM_LIST_ITEMS = 100;
-    // TODO (2) Create a GreenAdapter variable called mAdapter
+    // TODO (2)(ok) Create a GreenAdapter variable called mAdapter
     GreenAdapter mAdapter;
-    // TODO (3) Create a RecyclerView variable called mNumbersList
+    // TODO (3)(ok) Create a RecyclerView variable called mNumbersList
     RecyclerView mNumberList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        // TODO (4)(ok) Use findViewById to store a reference to the RecyclerView in mNumbersList
         mNumberList = (RecyclerView) findViewById(R.id.rv_numbers);
-        // TODO (5) Create a LinearLayoutManager variable called layoutManager
+        // TODO (5)(ok) Create a LinearLayoutManager variable called layoutManager
         LinearLayoutManager layoutManager = new LinearLayoutManager( this);
-        // TODO (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
+        // TODO (6)(ok) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
         mNumberList.setLayoutManager(layoutManager);
-        // TODO (7) Use setHasFixedSize(true) to designate that the contents of the RecyclerView won't change an item's size
+        // TODO (7)(ok) Use setHasFixedSize(true) to designate that the contents of the RecyclerView won't change an item's size
         mNumberList.setHasFixedSize(true);
-        // TODO (8) Store a new GreenAdapter in mAdapter and pass it NUM_LIST_ITEMS
+        // TODO (8)(ok) Store a new GreenAdapter in mAdapter and pass it NUM_LIST_ITEMS
         mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
-        // TODO (9) Set the GreenAdapter you created on mNumbersList
+        // TODO (9)(ok) Set the GreenAdapter you created on mNumbersList
         mNumberList.setAdapter(mAdapter);
     }
 }
