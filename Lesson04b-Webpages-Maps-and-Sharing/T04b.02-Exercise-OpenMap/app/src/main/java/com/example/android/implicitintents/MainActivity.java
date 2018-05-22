cde5f07aeab0ebfa7146e40bed1f2a6c1e673f42
William DEKOU <williamdekou@gmail.com>
2018-05-22 08:31:30 +0100

    Open google map from my app

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -48,15 +48,15 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        // TODO (5) Store an address in a String
+        // COMPLETED (5) Store an address in a String
         String address = "1600 Amphitheatre Parkway, CA";
-        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        // COMPLETED (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("geo")
                 .path("0,0")
                 .query(address);
         Uri uri = builder.build();
-        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
+        // COMPLETED (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
         showMap(uri);
     }
 
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickShareTextButton(View v) {
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "COMPLETED: Share text when this is clicked", Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void createYourOwn(View v) {
         Toast.makeText(this,
-                "TODO: Create Your Own Implicit Intent",
+                "COMPLETED: Create Your Own Implicit Intent",
                 Toast.LENGTH_SHORT)
                 .show();
     }
@@ -116,14 +116,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // TODO (1) Create a method called showMap with a Uri as the single parameter
+    // COMPLETED (1) Create a method called showMap with a Uri as the single parameter
     void showMap(Uri geo) {
         // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+        // COMPLETED (2) Create an Intent with action type, Intent.ACTION_VIEW
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        // TODO (3) Set the data of the Intent to the Uri passed into this method
+        // COMPLETED (3) Set the data of the Intent to the Uri passed into this method
         intent.setData(geo);
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        // COMPLETED (4) Verify that this Intent can be launched and then call startActivity
         if (intent.resolveActivity(getPackageManager()) != null){
             startActivity(intent);
         }
