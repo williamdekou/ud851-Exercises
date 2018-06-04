976aab66517a1b2db2ace3e8cf002d72aff1d418
William DEKOU <williamdekou@gmail.com>
2018-06-04 18:55:47 +0100

    Swippe to remove task!

--- Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -78,13 +79,14 @@ public class MainActivity extends AppCompatActivity implements
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 // Here is where you'll implement swipe to delete
 
-                // TODO (1) Construct the URI for the item to delete
+                // COMPLETED (1) Construct the URI for the item to delete
                 //[Hint] Use getTag (from the adapter code) to get the id of the swiped item
+                getContentResolver().delete(ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI, (Integer) viewHolder.itemView.getTag()), null, null);
+                // COMPLETED (2) Delete a single row of data using a ContentResolver
 
-                // TODO (2) Delete a single row of data using a ContentResolver
+                // COMPLETED (3) Restart the loader to re-query for all tasks after a deletion
 
-                // TODO (3) Restart the loader to re-query for all tasks after a deletion
-                
+                getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, MainActivity.this);
             }
         }).attachToRecyclerView(mRecyclerView);
 
